897f85e5a2f84ee4ebfe5c6230d2d370c742182e
echidnalover <echidnalover476@gmail.com>
2025-02-27 09:03:19 +0700

    Create index.js

---
+++ src/pages/pages/index.js
@@ -0,0 +1,5 @@
+export { default as Analytics } from "./analytics.js";
+export { default as Dashboard } from "./studysessions.js";
+export { default as Home } from "./home.js";
+export { default as StudyMaterials } from "./studymaterials.js";
+export { default as StudySessions } from "./studysessions.js";
