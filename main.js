baef44e484958343df8e7cb2f76e6bfefa5b44c9
echidnalover <echidnalover476@gmail.com>
2025-02-27 09:26:34 +0700

    Create main.js

---
+++ main.js
@@ -0,0 +1,10 @@
+// Import semua index.js dari masing-masing folder
+import "./src/components/ui/index.js";
+import "./src/pages/index.js";
+import "./src/apis/index.js";
+import "./src/ui/index.js";
+
+// Jalankan sesuatu saat website pertama kali dimuat
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Website Loaded!");
+});
